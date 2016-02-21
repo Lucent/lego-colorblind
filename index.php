619417772d2136a6aa1e0c303dab8e6454ebfcb7
Michael Dayah <michael@dayah.com>
2016-02-21 03:16:49 -0500

    Enable caching for one week.

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
  <head>
   <title>Differentiate Similar Color Lego Parts for Colorblindness</title>
   <link href="css/screen-default.css" rel="stylesheet" type="text/css">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
  </head>
  <body>
 <?
@@ -10,32 +11,61 @@ $similar_colors = [
 	[70, 308, 320],
 	[14, 25, 27],
 	[42, 46],
-	[308, 288],
-	[272, 288, 0]
+	[288, 308],
+	[0, 272, 288],
+	[1, 272]
 ];
+
+function get_set_json($id, $api_key) {
+	$request_params = [
+		"key" => $api_key,
+		"format" => "json",
+		"set" => $id
+	];
+
+	$cache_file = "cache" . DIRECTORY_SEPARATOR . $id;
+	if (file_exists($cache_file)) {
+		$fh = fopen($cache_file, "r");
+		$file_time = trim(fgets($fh));
+		if ($file_time > strtotime("-1 week"))
+			return fread($fh, filesize($cache_file));
+		else {
+			fclose($fh);
+			unlink($cache_file);
+		}
+	}
+
+	$request = "http://rebrickable.com/api/get_set_parts?" . http_build_query($request_params);
+	$set_json = file_get_contents($request);
+
+	if ($set_json == "NOSET")
+		return false;
+	else {
+		$fh = fopen($cache_file, "w");
+		fwrite($fh, time() . "\n");
+		fwrite($fh, $set_json);
+		fclose($fh);
+		return $set_json;
+	}
+}
 
 if (array_key_exists("set", $_GET)) {
 	if (strpos($_GET["set"], "-"))
 		$set_number = $_GET["set"];
 	else
 		$set_number = $_GET["set"] . "-1";
-	$request_params = [
-		"key" => $api_key,
-		"format" => "json",
-		"set" => $set_number
-	];
-	$request = "http://rebrickable.com/api/get_set_parts?" . http_build_query($request_params);
-	$set_json = json_decode(file_get_contents($request), true);
-	$set = $set_json[0];
-	if (count($set_json) === 0) {
+
+	$set_json = json_decode(get_set_json($set_number, $api_key), true);
+	if ($set_json === false) {
 		echo "Invalid set ID";
 		exit;
-	}
+	} else
+		$set = $set_json[0];
 }
 ?>
 <h1>Find parts that occur in multiple similar colors</h1>
 <?
-echo "<h1><img src='" . $set["set_img_url"] .  "'>" . $set["descr"];
+echo "<h1><img src='" . $set["set_img_url"] .  "'>" . htmlspecialchars_decode($set["descr"]);
 ?>
 <form method="get" action=".">
  <input type="text" name="set" placeholder="Set ID" value="<?= $set_number ?>">
