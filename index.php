73fb6f166b7d1d7490dfba1fd49d102d2a7ac35f
Michael Dayah <michael@dayah.com>
2016-02-22 22:46:40 -0500

    Allow different banks of similar colors, including Rebrickable's.

--- index.php
+++ index.php
@@ -8,12 +8,25 @@
 <?
 require "apikey.php";
 $similar_colors = [
-	[70, 308, 320],
-	[14, 25, 27],
-	[42, 46],
-	[288, 308],
-	[0, 272, 288],
-	[1, 272]
+	"Families" => [
+		[-1, 0, 32, 64, 75, 132, 133, 9999],
+		[1, 3, 9, 11, 23, 33, 41, 43, 52, 61, 73, 85, 89, 110, 112, 137, 143, 212, 232, 272, 313, 321, 322, 379, 1001, 1003],
+		[2, 10, 17, 21, 27, 34, 35, 42, 62, 74, 81, 115, 118, 120, 158, 288, 294, 323, 326, 378, 1002],
+		[4, 5, 12, 13, 20, 22, 26, 29, 30, 31, 36, 45, 63, 69, 77, 100, 114, 129, 216, 230, 236, 320, 335, 351, 373, 1007],
+		[6, 28, 70, 84, 86, 92, 134, 178, 308, 450],
+		[14, 18, 19, 25, 46, 54, 57, 68, 78, 82, 125, 142, 182, 191, 226, 297, 334, 366, 462, 484],
+		[15, 47, 79, 117, 151, 183, 1000],
+		[1006],
+		[7, 8, 40, 60, 71, 72, 76, 80, 135, 148, 150, 179, 383, 503],
+		[1004, 1005]
+	],
+	"Deuteranomaly" => [
+		[70, 308, 320],
+		[14, 25, 27],
+		[42, 46],
+		[288, 308],
+		[0, 1, 272, 288]
+	]
 ];
 
 function get_set_json($id, $api_key) {
@@ -49,6 +62,11 @@ function get_set_json($id, $api_key) {
 	}
 }
 
+$similar_color_bank = $similar_colors["Families"];
+if (array_key_exists("type", $_GET))
+	if (array_key_exists($_GET["type"], $similar_colors))
+		$similar_color_bank = $similar_colors[$_GET["type"]];
+
 if (array_key_exists("set", $_GET)) {
 	if (strpos($_GET["set"], "-"))
 		$set_number = $_GET["set"];
@@ -64,18 +82,20 @@ if (array_key_exists("set", $_GET)) {
 }
 ?>
 <h1>Find parts that occur in multiple similar colors</h1>
+<form method="get" action=".">
 <?
 echo "<h1><img src='" . $set["set_img_url"] .  "'>" . htmlspecialchars_decode($set["descr"]);
 ?>
-<form method="get" action=".">
  <input type="text" name="set" placeholder="Set ID" value="<?= $set_number ?>">
- <input type="submit" value="Show similar colors">
+</h1>
+<input type="radio" name="type" value="Deuteranomaly" id="Deuteranomaly" <?= ($_GET["type"] == "Deuteranomaly" ? "checked" : "") ?>><label for="Deuteranomaly">Red-Green</label>
+<input type="radio" name="type" value="Families" id="Families" <?= ($_GET["type"] == "Families" ? "checked" : "") ?>><label for="Families">Manuals</label>
+<input type="submit" value="Show similarly colored parts">
 </form>
-<?
-echo "</h1>\n\n";
 
+<?
 $parts_bydesign = [];
-foreach ($similar_colors as $color_pair_idx=>$color_pairs) {
+foreach ($similar_color_bank as $color_pair_idx=>$color_pairs) {
 	foreach ($set["parts"] as $part) {
 		if (in_array($part["ldraw_color_id"], $color_pairs) && $part["type"] == 1)
 			$parts_bydesign[$color_pair_idx][$part["part_id"]][] = $part;
